f96f105ff3c0ec38116ce4ac9a2a780844c19d56
elcio <elcio.taira@ps.otg.com.br>
2023-08-19 17:13:12 -0300

    feat: ad json server to dev, add config environments

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT;
+  if (!port) {
+    throw new InternalServerErrorException('Error to load environments');
+  }
+  await app.listen(port);
+  const logger = new Logger('Bootstrap');
+  logger.log(`Application is running on port ${port}`);
 }
 
 bootstrap();
