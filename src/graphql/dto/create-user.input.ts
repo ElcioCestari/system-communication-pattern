01c47d0d18c46c834204304e41aed099c58b5daf
elcio <elcio.taira@ps.otg.com.br>
2023-08-24 22:12:43 -0300

    refactor(graphql): rename doc

--- src/graphql/dto/create-user.input.ts
+++ src/graphql/dto/create-user.input.ts
@@ -2,18 +2,20 @@ import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => String, { description: 'Name of the service' })
+  @Field(() => String, { description: 'Name of the user' })
   name: string;
 
-  @Field(() => String, { description: 'Document of the service' })
+  @Field(() => String, { description: 'Document of the user' })
   document: string;
 
-  @Field(() => String, { description: 'Birthdate of the service' })
+  @Field(() => String, { description: 'Birthdate of the user' })
   birthDate: string;
 
-  @Field(() => [String], { description: 'List of phone numbers of the service' })
+  @Field(() => [String], {
+    description: 'List of phone numbers of the user',
+  })
   phones: string[];
 
-  @Field(() => [String], { description: 'List of addresses of the service' })
+  @Field(() => [String], { description: 'List of addresses of the user' })
   addresses: string[];
 }
