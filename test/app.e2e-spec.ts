b53dee720200a3cdce11f08b30c4a37c347e185f
elcio <elcio.taira@ps.otg.com.br>
2023-09-27 23:23:27 -0300

    test: refactor e2e to mock client

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,14 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { ClientModule } from '../src/client/client.module';
+import { UserClient } from '../src/client/user-client/user.client';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let clientMock = { get: jest.fn() };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      imports: [AppModule, ClientModule],
+    })
+      .overrideProvider(UserClient)
+      .useValue(clientMock)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
@@ -20,12 +26,12 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
+    clientMock.get.mockResolvedValueOnce([]);
     return request(app.getHttpServer())
       .get('/api/user')
       .expect(HttpStatus.OK)
       .expect((res) => {
-        console.log(res.body)
-        expect(res.body.length).toBeGreaterThan(0);
+        expect(res.body.length).toEqual(0);
       });
   });
 });
